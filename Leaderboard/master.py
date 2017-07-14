16aee928f52d5ad92180a2b95ca304874dd537bb
Austin Herrling <adherrling@gmail.com>
2017-07-14 15:28:44 -0400

    Cleaned up the million line of imports to just 2

--- Leaderboard/master.py
+++ Leaderboard/master.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python
 #Each function from BuildTables is a database function, and reads in .json files from the noted categories, then builds its named database table
 #Each function from APIRequests is an API request function, and pulls the corresponding .json files from Bungie's server.
-from buildTables import buildBungieTable
-from buildTables import buildDestinyTable
-from buildTables import buildStatTables
-from apiRequests import getClanUserJSONs
-from apiRequests import getIndividualUserJSONs
-from apiRequests import getUserStatsJSONs
+import buildTables as bt
+import apiRequests as ar
 from Tokens.getHeader import getHeader
 from Tokens.getClanId import getClanId
 import sys
@@ -33,17 +29,17 @@ clanId = getClanId(clanIdPath)
 
 #Start of function calls. Needs to follow the proper data flow to ensure that the next step always has the appropriate database or files that it needs.
 def fullSetup():
-    getClanUserJSONs(clan, header, clanId)     #Step 1 - Pull list of clan members
-    buildBungieTable(clan, db)                 #Step 2 - Build Bungie table, listing clan members
-    getIndividualUserJSONs(users, db, header)  #Step 3 - Pull user files, using Bungie table
-    buildDestinyTable(users, db)               #Step 4 - Build Destiny table, listing user Ids
-    getUserStatsJSONs(stats, db, header)       #Step 5 - Pull user stat files, using Destiny table
-    buildStatTables(stats, db)                 #Step 6 - Build stat tables, using user stat files
+    ar.getClanUserJSONs(clan, header, clanId)     #Step 1 - Pull list of clan members
+    bt.buildBungieTable(clan, db)                 #Step 2 - Build Bungie table, listing clan members
+    ar.getIndividualUserJSONs(users, db, header)  #Step 3 - Pull user files, using Bungie table
+    bt.buildDestinyTable(users, db)               #Step 4 - Build Destiny table, listing user Ids
+    ar.getUserStatsJSONs(stats, db, header)       #Step 5 - Pull user stat files, using Destiny table
+    bt.buildStatTables(stats, db)                 #Step 6 - Build stat tables, using user stat files
 
 def tablesOnly():
-    buildBungieTable(clan, db)                 #Step 2 - Build Bungie table, listing clan members
-    buildDestinyTable(users, db)               #Step 4 - Build Destiny table, listing user Ids
-    buildStatTables(stats, db)                 #Step 6 - Build stat tables, using user stat files
+    bt.buildBungieTable(clan, db)                 #Step 2 - Build Bungie table, listing clan members
+    bt.buildDestinyTable(users, db)               #Step 4 - Build Destiny table, listing user Ids
+    bt.buildStatTables(stats, db)                 #Step 6 - Build stat tables, using user stat files
 
 if __name__ == "__main__":
     args = sys.argv
