b9d0cd8f197f52ad1a8cd0454afc3b66ba25c45f
Austin <adherrling@gmail.com>
2017-09-11 16:03:21 -0400

    Started general stat tracking

--- testscript.py
+++ testscript.py
@@ -17,8 +17,10 @@ def testscript():
     # url = "Destiny2/{membershipType}/Profile/{destinyMembershipId}/?components=100"
     # url_end = "/Destiny2/{membershipType}/Account/{destinyMembershipId}/Character/{characterId}/Stats/?groups=3"
     # url_end = "/Destiny2/Stats/AggregateClanStats/1407546/"
-    # url_end = f"/Destiny2/{membershipType}/Account/{destinyMembershipId}/Stats/Leaderboards"
-    url_end = f"/Destiny2/{membershipType}/Account/{destinyMembershipId}/Character/{characterId}/Stats/Activities/?mode=story"
+    # url_end = f"/Destiny2/{membershipType}/Account/{destinyMembershipId}/Stats/"
+    url_end = f"/Destiny2/{membershipType}/Account/{destinyMembershipId}/Character/{characterId}/Stats"
+    # url_end = f"/Destiny2/{membershipType}/Account/{destinyMembershipId}/Character/{characterId}/Stats/Activities/?mode=control"
+    # url_end = f"/Destiny2/{membershipType}/Account/{destinyMembershipId}/Character/{characterId}/Stats/AggregateActivityStats/"
     # url = f"{URL_START}/GroupV2/{os.environ['BUNGIE_CLANID']}/Members/?currentPage=1"
     # Specify an outfile
     url = url_start + url_end
